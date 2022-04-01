53c5df127a64ae5d2b6c52c428110cacd572ad05
Extr3mist <102573042+Extr3mist@users.noreply.github.com>
2022-04-01 12:21:10 +0530

    Create wayback.sh

---
+++ wayback.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $2 == "time" ]
+then
+mkdir $1; cd $1; 
+curl "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" --max-time $3 >> "wayback-urls-$3.out" 
+grep -Po '.*?//\K.*?(?=/)' "wayback-urls-$3.out" | sort -u >> "wayback-subdomains-$3.out"
+else 
+mkdir $1; cd $1; 
+curl "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" >> wayback-urls.out 
+grep -Po '.*?//\K.*?(?=/)' wayback-urls.out | sort -u >> wayback-subdomains.out  # Extract only sub/domains
+fi
